ee64f5535bb3d69ff1da3db1893d3407e5c68f6a
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2024-03-02 14:20:16 +0530

    lint added

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi Ankit Chauhan 2023</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    height:"50px"
   },
 });
