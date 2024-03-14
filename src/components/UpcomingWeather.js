f92f625929b5d5adbf1ecda89d66af19db4b97a5
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2024-03-14 23:55:00 +0530

    working upcoming weather

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,11 +1,35 @@
 import React from 'react'
-import { StyleSheet, Text } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Feather } from '@expo/vector-icons'
 
-const UpcomingWeather = () => {
+const Item = ({ props }) => {
+    const { id, title } = props
     return (
-        <SafeAreaView>
-            <Text>Upcoming weather</Text>
+        <View>
+            <Feather name={'sun'} size={50} color={'white'} />
+            <Text>The Titile is : {title}</Text>
+            <Text>It's Id is : {id}</Text>
+        </View>
+    )
+}
+const UpcomingWeather = () => {
+    const renderItem = ({ item }) => {
+        return <Item props={item} />
+    }
+    const DATA = [
+        { id: '1', title: 'First Item' },
+        { id: '2', title: 'Second Item' },
+        { id: '3', title: 'Third Item' },
+        { id: '4', title: 'Fourth Item' }
+    ]
+    return (
+        <SafeAreaView styles={styles.container}>
+            <Text>Upcoming Weather</Text>
+            <FlatList
+                data={DATA}
+                renderItem={renderItem }
+            />
         </SafeAreaView>
     )
 }
